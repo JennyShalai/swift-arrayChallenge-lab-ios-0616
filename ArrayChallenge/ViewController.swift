57490bb3817864cc99e01b6f8c2013a2cf6e9cf6
JennyShalai <jenny.shalai@gmail.com>
2016-07-07 13:45:03 -0400

    tests passed but light grey

--- ArrayChallenge/ViewController.swift
+++ ArrayChallenge/ViewController.swift
@@ -15,11 +15,22 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let arrayOfNumbers = [6, 4, 12, 4]
+        let arrayOfItems = ["Bananas", "Apples", "Eggs", "Rolls"]
     
         
-        
+        shoppingList = makeShoppingList(arrayOfItems, quantityOfItems: arrayOfNumbers)
     }
     
+    func makeShoppingList(itemsNeeded:[String], quantityOfItems:[Int]) -> [String] {
+        var resultArray: [String] = []
+        if quantityOfItems.count == itemsNeeded.count {
+            for (index, item) in itemsNeeded.enumerate() {
+                resultArray.append("\(index + 1). \(quantityOfItems[index]) \(item)")
+            }
+        }
+        return resultArray
+    }
     
     
 }
